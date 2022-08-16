555779423240e77e2ce4a044d383342c9da5627a
chmehar143 <chmehar143@gmail.com>
2022-08-16 21:48:31 +0500

    move the file from frontend folder to user floder

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -25,4 +25,35 @@ class CategoryController extends Controller
     {
         return view('admin.Category.view');
     }
+
+    //*** POST Request
+    public function store(Request $request)
+    {
+        //--- Validation Section
+        $rules = [
+            'name' => 'unique:blog_categories',
+            'slug' => 'unique:blog_categories'
+        ];
+        $customs = [
+            'name.unique' => 'This name has already been taken.',
+            'slug.unique' => 'This slug has already been taken.'
+        ];
+        $validator = Validator::make($request->all(), $rules, $customs);
+        if ($validator->fails()) {
+            return response()->json(array('errors' => $validator->getMessageBag()->toArray()));
+        }
+        //--- Validation Section Ends
+
+        //--- Logic Section
+        $data = new BlogCategory;
+        $input = $request->all();
+        $data->fill($input)->save();
+        //--- Logic Section Ends
+
+        //--- Redirect Section
+        $msg = 'New Data Added Successfully.';
+        return response()->json($msg);
+        //--- Redirect Section Ends
+    }
+
 }
