2c9b590ab0ac784500fed3f552ba8a9437af5713
chmehar143 <chmehar143@gmail.com>
2022-10-22 00:43:47 +0500

    "chat bot"

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -8,19 +8,24 @@ use Illuminate\Http\Request;
 
 class BotManController extends Controller
 {
+    private static $bar;
+    public function __construct()
+    {
+        self::$bar = false;
+    }
+
     public function handle()
     {
         $botman = app('botman');
 
         $botman->hears('{message}',function($botman,$message){
-
-            if ($message == 'hi'|| $message =='hello'|| $message =='salam'|| $message =='as'|| $message =='assalam o alaikum') {
+            $message = strtolower($message);
+            if ($message == 'hi'|| $message == 'hy'|| $message =='hello'|| $message =='salam'|| $message =='as'|| $message =='assalam o alaikum') {
                 $this->askName($botman);
-
-            }else{
+            }
+            else{
                 $botman->reply("please say 'hi', or 'assalam o alaikum' for start chat ");
             }
-
         });
 
         $botman->listen();
@@ -28,9 +33,16 @@ class BotManController extends Controller
 
     public function askName($botman)
     {
-        $botman->ask("Great! What is Your Name?",function(Answer $answer){
+        $botman->ask("Great! What is Your Name?",function(Answer $answer,$botman){
             $name = $answer->getText();
             $this->say('Nice to meet you '.$name);
+            $botman->ask("what the issue you want to discuss with us please share details", function (Answer $answer) {
+                // this $msg need to save into database
+                $msg = $answer->getText();
+                $this->say('thanks! we will connect you soon');
+                $this->say('you can also used contact us page for any further query');
+            });
         });
     }
+
 }
