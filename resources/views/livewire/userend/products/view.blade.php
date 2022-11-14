702bd9a5c5069cd619a49e92e976002ecf3249ae
Afzaal565 <afzaalhussain565@gmail.com>
2022-11-14 09:46:45 -0500

    new updates by afzaal

--- resources/views/livewire/userend/products/view.blade.php
+++ resources/views/livewire/userend/products/view.blade.php
@@ -487,56 +487,11 @@
                               <li class="comment">
                                  <div class="comment-body">
                                     <figure class="comment-avatar">
-                                       <img src="assets/images/agents/2-100x100.png"
+                                       <img src=""
                                           alt="Commenter Avatar" width="90" height="90">
                                     </figure>
                                     <div class="comment-content">
-                                       <h4 class="comment-author">
-                                          <a href="#">John Doe</a>
-                                          <span class="comment-date">March 22, 2021 at 1:52
-                                          pm</span>
-                                       </h4>
-                                       <div class="ratings-container comment-rating">
-                                          <div class="ratings-full">
-                                             <span class="ratings" style="width: 80%;"></span>
-                                             <span class="tooltiptext tooltip-top"></span>
-                                          </div>
-                                       </div>
-                                       <p>Nullam a magna porttitor, dictum risus nec, faucibus
-                                          sapien.
-                                          Ultrices eros in cursus turpis massa tincidunt ante in
-                                          nibh mauris cursus mattis.
-                                          Cras ornare arcu dui vivamus arcu felis bibendum ut
-                                          tristique.
-                                       </p>
-                                       <!-- <div class="comment-action">
-                                          <a href="#"
-                                             class="btn btn-secondary btn-link btn-underline sm btn-icon-left font-weight-normal text-capitalize">
-                                          <i class="far fa-thumbs-up"></i>Helpful (1)
-                                          </a>
-                                          <a href="#"
-                                             class="btn btn-dark btn-link btn-underline sm btn-icon-left font-weight-normal text-capitalize">
-                                          <i class="far fa-thumbs-down"></i>Unhelpful (0)
-                                          </a>
-                                          <div class="review-image">
-                                             <a href="#">
-                                                <figure>
-                                                   <img src="{{asset('frontend-assets/assets/images/products/default/review-img-2.jpg')}}"
-                                                      width="60" height="60"
-                                                      alt="Attachment image of John Doe's review on Electronics Black Wrist Watch"
-                                                      data-zoom-image="{{asset('frontend-assets/assets/images/products/default/review-img-2-800x900.jpg')}}" />
-                                                </figure>
-                                             </a>
-                                             <a href="#">
-                                                <figure>
-                                                   <img src="{{asset('frontend-assets/assets/images/products/default/review-img-3.jpg')}}"
-                                                      width="60" height="60"
-                                                      alt="Attachment image of John Doe's review on Electronics Black Wrist Watch"
-                                                      data-zoom-image="{{asset('frontend-assets/assets/images/products/default/review-img-3-800x900.jpg')}}" />
-                                                </figure>
-                                             </a>
-                                          </div>
-                                       </div> -->
+                                       <h3>No comment yet</h3>
                                     </div>
                                  </div>
                               </li>
@@ -592,56 +547,11 @@
                               <li class="comment">
                                  <div class="comment-body">
                                     <figure class="comment-avatar">
-                                       <img src="assets/images/agents/2-100x100.png"
+                                       <img src=""
                                           alt="Commenter Avatar" width="90" height="90">
                                     </figure>
                                     <div class="comment-content">
-                                       <h4 class="comment-author">
-                                          <a href="#">John Doe</a>
-                                          <span class="comment-date">March 22, 2021 at 1:52
-                                          pm</span>
-                                       </h4>
-                                       <div class="ratings-container comment-rating">
-                                          <div class="ratings-full">
-                                             <span class="ratings" style="width: 80%;"></span>
-                                             <span class="tooltiptext tooltip-top"></span>
-                                          </div>
-                                       </div>
-                                       <p>Nullam a magna porttitor, dictum risus nec, faucibus
-                                          sapien.
-                                          Ultrices eros in cursus turpis massa tincidunt ante in
-                                          nibh mauris cursus mattis.
-                                          Cras ornare arcu dui vivamus arcu felis bibendum ut
-                                          tristique.
-                                       </p>
-                                       <!-- <div class="comment-action">
-                                          <a href="#"
-                                             class="btn btn-secondary btn-link btn-underline sm btn-icon-left font-weight-normal text-capitalize">
-                                          <i class="far fa-thumbs-up"></i>Helpful (1)
-                                          </a>
-                                          <a href="#"
-                                             class="btn btn-dark btn-link btn-underline sm btn-icon-left font-weight-normal text-capitalize">
-                                          <i class="far fa-thumbs-down"></i>Unhelpful (0)
-                                          </a>
-                                          <div class="review-image">
-                                             <a href="#">
-                                                <figure>
-                                                   <img src="{{asset('frontend-assets/assets/images/products/default/review-img-2.jpg')}}"
-                                                      width="60" height="60"
-                                                      alt="Attachment image of John Doe's review on Electronics Black Wrist Watch"
-                                                      data-zoom-image="{{asset('frontend-assets/assets/images/products/default/review-img-2-800x900.jpg')}}" />
-                                                </figure>
-                                             </a>
-                                             <a href="#">
-                                                <figure>
-                                                   <img src="{{asset('frontend-assets/assets/images/products/default/review-img-3.jpg')}}"
-                                                      width="60" height="60"
-                                                      alt="Attachment image of John Doe's review on Electronics Black Wrist Watch"
-                                                      data-zoom-image="{{asset('frontend-assets/assets/images/products/default/review-img-3-800x900.jpg')}}" />
-                                                </figure>
-                                             </a>
-                                          </div>
-                                       </div> -->
+                                       <h3>No comment yet</h3>
                                     </div>
                                  </div>
                               </li>
@@ -697,56 +607,11 @@
                               <li class="comment">
                                  <div class="comment-body">
                                     <figure class="comment-avatar">
-                                       <img src="assets/images/agents/2-100x100.png"
+                                       <img src=""
                                           alt="Commenter Avatar" width="90" height="90">
                                     </figure>
                                     <div class="comment-content">
-                                       <h4 class="comment-author">
-                                          <a href="#">John Doe</a>
-                                          <span class="comment-date">March 22, 2021 at 1:52
-                                          pm</span>
-                                       </h4>
-                                       <div class="ratings-container comment-rating">
-                                          <div class="ratings-full">
-                                             <span class="ratings" style="width: 80%;"></span>
-                                             <span class="tooltiptext tooltip-top"></span>
-                                          </div>
-                                       </div>
-                                       <p>Nullam a magna porttitor, dictum risus nec, faucibus
-                                          sapien.
-                                          Ultrices eros in cursus turpis massa tincidunt ante in
-                                          nibh mauris cursus mattis.
-                                          Cras ornare arcu dui vivamus arcu felis bibendum ut
-                                          tristique.
-                                       </p>
-                                       <!-- <div class="comment-action">
-                                          <a href="#"
-                                             class="btn btn-secondary btn-link btn-underline sm btn-icon-left font-weight-normal text-capitalize">
-                                          <i class="far fa-thumbs-up"></i>Helpful (1)
-                                          </a>
-                                          <a href="#"
-                                             class="btn btn-dark btn-link btn-underline sm btn-icon-left font-weight-normal text-capitalize">
-                                          <i class="far fa-thumbs-down"></i>Unhelpful (0)
-                                          </a>
-                                          <div class="review-image">
-                                             <a href="#">
-                                                <figure>
-                                                   <img src="{{asset('frontend-assets/assets/images/products/default/review-img-2.jpg')}}"
-                                                      width="60" height="60"
-                                                      alt="Attachment image of John Doe's review on Electronics Black Wrist Watch"
-                                                      data-zoom-image="{{asset('frontend-assets/assets/images/products/default/review-img-2-800x900.jpg')}}" />
-                                                </figure>
-                                             </a>
-                                             <a href="#">
-                                                <figure>
-                                                   <img src="{{asset('frontend-assets/assets/images/products/default/review-img-3.jpg')}}"
-                                                      width="60" height="60"
-                                                      alt="Attachment image of John Doe's review on Electronics Black Wrist Watch"
-                                                      data-zoom-image="{{asset('frontend-assets/assets/images/products/default/review-img-3-800x900.jpg')}}" />
-                                                </figure>
-                                             </a>
-                                          </div>
-                                       </div> -->
+                                       <h3>No comment yet</h3>
                                     </div>
                                  </div>
                               </li>
@@ -802,56 +667,11 @@
                               <li class="comment">
                                  <div class="comment-body">
                                     <figure class="comment-avatar">
-                                       <img src="assets/images/agents/2-100x100.png"
+                                       <img src=""
                                           alt="Commenter Avatar" width="90" height="90">
                                     </figure>
                                     <div class="comment-content">
-                                       <h4 class="comment-author">
-                                          <a href="#">John Doe</a>
-                                          <span class="comment-date">March 22, 2021 at 1:52
-                                          pm</span>
-                                       </h4>
-                                       <div class="ratings-container comment-rating">
-                                          <div class="ratings-full">
-                                             <span class="ratings" style="width: 80%;"></span>
-                                             <span class="tooltiptext tooltip-top"></span>
-                                          </div>
-                                       </div>
-                                       <p>Nullam a magna porttitor, dictum risus nec, faucibus
-                                          sapien.
-                                          Ultrices eros in cursus turpis massa tincidunt ante in
-                                          nibh mauris cursus mattis.
-                                          Cras ornare arcu dui vivamus arcu felis bibendum ut
-                                          tristique.
-                                       </p>
-                                       <!-- <div class="comment-action">
-                                          <a href="#"
-                                             class="btn btn-secondary btn-link btn-underline sm btn-icon-left font-weight-normal text-capitalize">
-                                          <i class="far fa-thumbs-up"></i>Helpful (1)
-                                          </a>
-                                          <a href="#"
-                                             class="btn btn-dark btn-link btn-underline sm btn-icon-left font-weight-normal text-capitalize">
-                                          <i class="far fa-thumbs-down"></i>Unhelpful (0)
-                                          </a>
-                                          <div class="review-image">
-                                             <a href="#">
-                                                <figure>
-                                                   <img src="{{asset('frontend-assets/assets/images/products/default/review-img-2.jpg')}}"
-                                                      width="60" height="60"
-                                                      alt="Attachment image of John Doe's review on Electronics Black Wrist Watch"
-                                                      data-zoom-image="{{asset('frontend-assets/assets/images/products/default/review-img-2-800x900.jpg')}}" />
-                                                </figure>
-                                             </a>
-                                             <a href="#">
-                                                <figure>
-                                                   <img src="{{asset('frontend-assets/assets/images/products/default/review-img-3.jpg')}}"
-                                                      width="60" height="60"
-                                                      alt="Attachment image of John Doe's review on Electronics Black Wrist Watch"
-                                                      data-zoom-image="{{asset('frontend-assets/assets/images/products/default/review-img-3-800x900.jpg')}}" />
-                                                </figure>
-                                             </a>
-                                          </div>
-                                       </div> -->
+                                       <h3>No comment yet</h3>
                                     </div>
                                  </div>
                               </li>
