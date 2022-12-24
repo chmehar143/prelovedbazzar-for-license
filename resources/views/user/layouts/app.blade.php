0e24e8e8ceca47f0eafda518c9d89835bbe9f02f
unknown <johneswilliam28@gmail.com>
2022-12-25 00:10:50 +0500

    update

--- resources/views/user/layouts/app.blade.php
+++ resources/views/user/layouts/app.blade.php
@@ -63,7 +63,7 @@ else{
 
     <!-- Vendor CSS -->
     <link rel="stylesheet" type="text/css" href="{{asset('frontend-assets/assets/vendor/fontawesome-free/css/all.min.css')}}">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Plugins CSS -->
     <!-- <link rel="stylesheet" href="assets/vendor/swiper/swiper-bundle.min.css"> -->
     <link rel="stylesheet" type="text/css" href="{{asset('frontend-assets/assets/vendor/animate/animate.min.css')}}">
